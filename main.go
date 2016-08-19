aea9d53e9028f56580b8bc1e9d67b87573498cfb
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-08-19 20:22:08 +0300

    make https client for getting information from github api

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 	"github.com/zenazn/goji/web/middleware"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 type Repos []string
 
+func getRepos() ([]byte, error) {
+	tr := &http.Transport{
+		TLSClientConfig:    &tls.Config{RootCAs: nil},
+		DisableCompression: false,
+	}
+	client := &http.Client{Transport: tr}
+
+	response, err := client.Get("https://api.github.com/users/yonidavidson/repos")
+	if err != nil {
+		log.Println(err)
+		return []byte(""), err
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	return body, err
+}
+
 func UserHandler(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `["yonidavidson.github.io", "DUMMY2"]`)
+	repos, err := getRepos()
+	if err != nil {
+		fmt.Fprintf(w, string(repos))
+	}
+	fmt.Fprintf(w, "Failed to get data", 500)
 }
 
 func main() {
