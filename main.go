183623e12690bf373c6680f6832c6a4d8cf3903d
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-08-19 03:13:18 +0300

    Created a simple http handler with
    paramter :username
    fixes #1

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
+	"github.com/zenazn/goji/web/middleware"
 )
 
 func hello(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["username"])
 }
 
 func main() {
-	goji.Get("/hello/:name", hello)
+	goji.Get("/api/gh/:username", hello)
+	goji.Use(middleware.EnvInit)
 	goji.Serve()
 }
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/zenazn/goji/web"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetHelloHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/gh/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := web.HandlerFunc(hello)
+
+	urlparams := make(map[string]string)
+	urlparams["username"] = "yoni"
+	ctx := web.C{urlparams, make(map[interface{}]interface{})}
+
+	handler.ServeHTTPC(ctx, rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "Hello, yoni!"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+}
