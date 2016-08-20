98f1337b325ec15f5e7a8a97e14cc4f407f214be
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-08-20 20:43:36 +0300

    moved userHandler to handlers.go

--- app/main.go
+++ app/main.go
@@ -1,51 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/zenazn/goji"
-	"github.com/zenazn/goji/web"
 	"github.com/zenazn/goji/web/middleware"
-	"log"
-	"net/http"
-	"sort"
 )
 
 var MODE_DRY bool = false
-
-func getUserData(name string) (Repos, error) {
-	var r Repos
-	var body []byte
-	var err error
-	if MODE_DRY {
-		body, err = getDataFromLocal()
-	} else {
-		body, err = getBody(name)
-	}
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	err = json.Unmarshal(body, &r)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	s := r._map(scorer, nil)
-	m := s._map(extractor, []string{"name", "score"})
-	sort.Sort(ByScore(m))
-	return m, nil
-}
-
-func UserHandler(c web.C, w http.ResponseWriter, r *http.Request) {
-	data, err := getUserData(c.URLParams["username"])
-	if err != nil {
-		fmt.Fprintf(w, "Failed to get data:", 500)
-	} else {
-		json, _ := json.Marshal(data)
-		fmt.Fprintf(w, "%v", string(json))
-	}
-}
 
 func main() {
 	goji.Get("/api/gh/:username", UserHandler)
